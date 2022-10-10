154d1910554e0da0b401df9eeb429b3296d21ec5
edweenlo <43148042+edweenlo@users.noreply.github.com>
2022-10-10 14:32:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,45 @@ function onDataParseHandler() {
     let modifiedOrders = [];
     let cachedItems = {};
     
-    
+    Object.keys(groupByOrderId).forEach((orderId) => {
+      let orders = groupByOrderId[orderId];
+
+      // if wo1 and wo2 are missing consider them completed
+      let woOneComplete = orders.some((order) => {
+        return order.workordernum !== 'Work Order 1';
+        
+      });
+      let woTwoComplete = orders.some((order) => {
+        return order.workordernum !== 'Work Order 2';
+      });
+
+      // if wo1 or wo2 have enough available to meet qtyconversion demand...
+      let woOneEnoughAvailable = orders.every((order) => {
+        if (order.workordernum === 'Work Order 1') {
+          return order.available > order.qtyconversion;
+        }
+      });
+      let woTwoEnoughAvailable = orders.every((order) => {
+        if (order.workordernum === 'Work Order 2') {
+          return order.available > order.qtyconversion;
+        }
+      });
+
+      orders.forEach((order) => {
+        if (woOneComplete || woOneEnoughAvailable) {
+          order.woOneFulfillable = true;
+        } 
+        if (woTwoComplete || woTwoEnoughAvailable) {
+          order.woTwoFulfillable = true;
+        } 
+      });
+
+
+    });
+
+
+
+
     Object.keys(groupByOrderId).forEach((orderId) => {
       let orders = groupByOrderId[orderId];
       
@@ -41,14 +79,24 @@ function onDataParseHandler() {
 
       let canFulfil = true;
 
+
       temp.forEach((order) => {
+        if (order.woOneFulfillable && order.inventorytype === 'Work In Process') {
+          order.available += order.qtyconversion;
+          order.qtyAdded = true;
+        }
+        if (order.woTwoFulfillable && order.inventorytype === 'Finished Goods') {
+          order.available += order.qtyconversion;
+          order.qtyAdded = true;
+        }
+
+
         if (
           order.available === "NULL" ||
           Number(order.qtyconversion) > Number(order.available)
         ) {
           canFulfil = false;
           order.status = "red";
-          
         }
       });
 
@@ -57,23 +105,29 @@ function onDataParseHandler() {
         if (!order.status) {
           order.status = canFulfil ? "green" : "none";
         }
-
         modifiedOrders.push(order);
       });
       
-
+      
       if (canFulfil) {
         temp.forEach((order) => {
           if (!cachedItems[order.item]) {
             cachedItems[order.item] =
               Number(order.available) - Number(order.qtyconversion);
           } else {
+            
             let availableForItem = cachedItems[order.item];
-
             order.available = Number(availableForItem);
-
             cachedItems[order.item] =
               Number(order.available) - Number(order.qtyconversion);
+
+
+              if (order.woOneFulfillable && order.inventorytype === 'Work In Process') {
+                order.available += order.qtyconversion;
+              }
+              if (order.woTwoFulfillable && order.inventorytype === 'Finished Goods') {
+                order.available += order.qtyconversion;
+              }  
           }
         });
       } else {
@@ -89,30 +143,30 @@ function onDataParseHandler() {
     
 
     for (let index = 0; index < modifiedOrders.length; index++) {
-      const { date, productionorder, batch, location, workordernum, order, item, description, inventorytype, units, quantity, backordered, committed, qtyconversion, pmtacommit, nonpmtacommit, ohqnow, available, status } =
+      const { date, productionorder, batch, location, workordernum, order, item, description, inventorytype, units, quantity, backordered, committed, qtyconversion, pmtacommit, nonpmtacommit, ohqnow, available, status, qtyAdded } =
         modifiedOrders[index];
       
       output += `
         <tr class="${status}">
-          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? '59CD90' : status === 'red' ? 'EE6352' : 'ffffff'}" class="date">${date}</td>
-          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? '59CD90' : status === 'red' ? 'EE6352' : 'ffffff'}" class="productionorder">${productionorder}</td>
-          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? '59CD90' : status === 'red' ? 'EE6352' : 'ffffff'}" class="batch">${batch}</td>
-          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? '59CD90' : status === 'red' ? 'EE6352' : 'ffffff'}" class="location">${location}</td>
-          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? '59CD90' : status === 'red' ? 'EE6352' : 'ffffff'}" class="workordernum">${workordernum}</td>
-          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? '59CD90' : status === 'red' ? 'EE6352' : 'ffffff'}" class="orders">${order}</td>
-          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? '59CD90' : status === 'red' ? 'EE6352' : 'ffffff'}" class="item">${item}</td>
-          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? '59CD90' : status === 'red' ? 'EE6352' : 'ffffff'}" class="description">${description}</td>
-          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? '59CD90' : status === 'red' ? 'EE6352' : 'ffffff'}" class="inventoryType">${inventorytype}</td>
-          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? '59CD90' : status === 'red' ? 'EE6352' : 'ffffff'}" class="units">${units}</td>
-          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? '59CD90' : status === 'red' ? 'EE6352' : 'ffffff'}" class="quantity">${quantity}</td>
-          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? '59CD90' : status === 'red' ? 'EE6352' : 'ffffff'}" class="backordered">${backordered}</td>
-          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? '59CD90' : status === 'red' ? 'EE6352' : 'ffffff'}" class="committed">${committed}</td>
-          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? '59CD90' : status === 'red' ? 'EE6352' : 'ffffff'}" class="qtyconversion">${qtyconversion}</td>
-          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? '59CD90' : status === 'red' ? 'EE6352' : 'ffffff'}" class="pmtacommit">${pmtacommit}</td>
-          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? '59CD90' : status === 'red' ? 'EE6352' : 'ffffff'}" class="nonpmtacommit">${nonpmtacommit}</td>
-          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? '59CD90' : status === 'red' ? 'EE6352' : 'ffffff'}" class="ohqnow">${ohqnow}</td>
-          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? '59CD90' : status === 'red' ? 'EE6352' : 'ffffff'}" class="available">${available}</td>
-          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? '59CD90' : status === 'red' ? 'EE6352' : 'ffffff'}" class="schedule">${status === 'green' ? 'true' : 'false'}</td>
+          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? 'D4F4DD' : status === 'red' ? 'D62246' : 'ffffff'}" class="date">${date}</td>
+          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? 'D4F4DD' : status === 'red' ? 'D62246' : 'ffffff'}" class="productionorder">${productionorder}</td>
+          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? 'D4F4DD' : status === 'red' ? 'D62246' : 'ffffff'}" class="batch">${batch}</td>
+          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? 'D4F4DD' : status === 'red' ? 'D62246' : 'ffffff'}" class="location">${location}</td>
+          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? 'D4F4DD' : status === 'red' ? 'D62246' : 'ffffff'}" class="workordernum">${workordernum}</td>
+          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? 'D4F4DD' : status === 'red' ? 'D62246' : 'ffffff'}" class="orders">${order}</td>
+          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? 'D4F4DD' : status === 'red' ? 'D62246' : 'ffffff'}" class="item">${item}</td>
+          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? 'D4F4DD' : status === 'red' ? 'D62246' : 'ffffff'}" class="description">${description}</td>
+          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? 'D4F4DD' : status === 'red' ? 'D62246' : 'ffffff'}" class="inventoryType">${inventorytype}</td>
+          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? 'D4F4DD' : status === 'red' ? 'D62246' : 'ffffff'}" class="units">${units}</td>
+          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? 'D4F4DD' : status === 'red' ? 'D62246' : 'ffffff'}" class="quantity">${quantity}</td>
+          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? 'D4F4DD' : status === 'red' ? 'D62246' : 'ffffff'}" class="backordered">${backordered}</td>
+          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? 'D4F4DD' : status === 'red' ? 'D62246' : 'ffffff'}" class="committed">${committed}</td>
+          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? 'D4F4DD' : status === 'red' ? 'D62246' : 'ffffff'}" class="qtyconversion">${qtyconversion}</td>
+          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? 'D4F4DD' : status === 'red' ? 'D62246' : 'ffffff'}" class="pmtacommit">${pmtacommit}</td>
+          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? 'D4F4DD' : status === 'red' ? 'D62246' : 'ffffff'}" class="nonpmtacommit">${nonpmtacommit}</td>
+          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? 'D4F4DD' : status === 'red' ? 'D62246' : 'ffffff'}" class="ohqnow">${ohqnow}</td>
+          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? 'D4F4DD' : status === 'red' ? 'D62246' : 'ffffff'}" class="available" style="color: ${qtyAdded ? '#0E7C7B' : 'black'}">${available}</td>
+          <td data-b-a-s="thin" data-fill-color="${status === 'green' ? 'D4F4DD' : status === 'red' ? 'D62246' : 'ffffff'}" class="schedule">${status === 'green' ? 'true' : 'false'}</td>
         </tr>
       `;
     }
